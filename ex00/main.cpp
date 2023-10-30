cb240770673d9f9e4abb3ec82e4adc469f5819c8
akazuki <kazuki200317kazu@gmail.com>
2023-10-30 17:54:55 +0900

    cpp01

---
+++ ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "Zombie.hpp"
+
+int main() {
+    Zombie *new_zombie;
+
+    randomChamp("akazuki");
+    new_zombie = newZombie("akazukin");
+    new_zombie->announce();
+    randomChamp("akazuking");
+    delete(new_zombie);
+}
---
+++ ex01/zomieHorde.cpp
@@ -0,0 +1,7 @@
+#include "Zombie.hpp"
+
+Zombie *zombieHorde(int N, std::string name) {
+    Zombie *new_zombie_array;
+
+    new_zombie_array = new Zombie[N];
+}
