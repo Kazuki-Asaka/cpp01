4e658f1b7e88b43480172ab522682e0d1d57b0de
Asaka Kazuki <akazuki@c1r23s11.42tokyo.jp>
2023-11-04 21:34:29 +0900

    re

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::string str;
+void	changeValue(std::string& str)
+{
+	str = "newvalue";
+}
 
-    str = "HI THIS IS BRAIN";
-    std::cout << str << std::endl;
+int main() {
+	std::string str;
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	str = "HI THIS IS BRAIN";
+	std::cout << str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << "ref;" << &stringREF << std::endl;
+
+	std::cout << "------------------------------------------" << std::endl;
+	std::string str1;
+	str1 = "hi";
+	std::cout << str1 << std::endl;
+	stringPTR = &str1;
+	std::cout << stringPTR << std::endl;
+	stringREF = str1;
+	changeValue(str);
+	std::cout << "ref;" << &stringREF << std::endl;
+	std::cout << "refstr;" << stringREF << std::endl;
 }
